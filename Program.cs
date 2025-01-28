803d88adb9ab9d47446a4583eb1fe7321ad9c374
LebbonJ <justin.lebbon@students.snow.edu>
2025-01-27 18:33:25 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Clear();
+
+Console.BackgroundColor = ConsoleColor.DarkCyan;
+Console.SetCursorPosition(0,0);
